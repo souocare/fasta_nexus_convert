3f47b6b65fe22f1156b5e7d4eaf680a62702d178
souocare <38910349+souocare@users.noreply.github.com>
2019-05-27 14:21:09 +0100

    added doc string. deleted unnecessary global variable

--- divide_keys_values.py
+++ divide_keys_values.py
@@ -1,6 +1,11 @@
 from itertools import groupby
 
 def fasta_rearrange(fasta):
+    '''
+    Procurar todos os nomes e sequências, e colocar num dicionário, a partir do ficheiro fasta que é aceite como argumento
+    :param fasta: recebe o ficheiro fasta
+    :return:
+    '''
     with open(fasta, "r") as fasta:
         groups = groupby(fasta, key=lambda x: not x.startswith(">")) # procura os nomes começados por >
         fasta_file = {} # cria um dicionario vazio
@@ -8,7 +13,5 @@ def fasta_rearrange(fasta):
             if not k:
                 key, val = list(v)[0].replace('\n', ''), "".join(map(str.rstrip, next(groups)[1])) #em cada valor da lista, o 'enter' é substituido por nada				
                 fasta_file[key] = val # coloca no dicionarios os valores e as chaves
-
-    global novo # cria uma variavel global novo
 
     novo = {k.replace('>', ''): v for k, v in fasta_file.items()} # faz replace ao > por nada
