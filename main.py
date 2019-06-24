1296be2b084045721e35c23f35cda1d0462804a7
souocare <38910349+souocare@users.noreply.github.com>
2019-06-24 14:23:58 +0100

    created variables to use with the return variables of the functions

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ outgroup = argv[3]
 #em cima, obtem-se as variáveis do user
 
 
-divide_keys_values.fasta_rearrange(fasta_file_from_user)
+new_key_values = divide_keys_values.fasta_rearrange(fasta_file_from_user)
 #corre o module 'divide_keys_values', correndo a função 'fasta_rearrange', dando à funçãoo, a variável fasta_file_from_user. Quando o programa correr, vai ser obtida uma variável 'novo'
 
-cabecalho.cabecalho(divide_keys_values.novo)
+new_header = cabecalho.cabecalho(new_key_values)
 #corre o module 'cabecalho', correndo a função 'cabecalho', dando à função a variável obtida da função acima, obtendo a variável 'start_nexus'
 
-sequences_writter.seq_writter(divide_keys_values.novo)
+new_sequence = sequences_writter.seq_writter(new_key_values)
 #corre o module 'sequences_writter', correndo a função 'seq_writter', dando à função a variável obtida da função acima, obtendo a variável 'sequences'
 
-mrbayes_blockwriter(ngen, outgroup)
+mb_blockwritter = mrbayes_blockwriter(ngen, outgroup)
 #corre o module 'mrbayes', correndo a função 'mrbayes_blockwriter', dando à função a variável 'ngen' e 'outgroup' obtidas inicialmente, obtendo a variável 'mrbayes_final'
 
 
-nexus_writter= cabecalho.start_nexus + sequences_writter.sequences + mrbayes.mrbayes_final
+nexus_writter= new_header + new_header + mb_blockwritter
 #na variavel nexus_writter, juntamos as várias variáveis obtidas anteriormente
 
 nexus_file= open("nexus_file.nex", "w+")
@@ -30,6 +30,8 @@ nexus_file= open("nexus_file.nex", "w+")
 
 nexus_file.write(nexus_writter)
 #escrever a variável 'nexus_writter' no ficheiro
+
+print(nexus_file)
 
 nexus_file.close()
 #fecha o ficheiro
